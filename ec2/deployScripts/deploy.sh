c7a1ec6e7e4be7f0b612f0edfb229941f2b732a2
Ryan Henao <ryanbhenao@gmail.com>
2024-04-19 15:57:07 +0000

    updated deploy scripts with ECR image URI

--- ec2/deployScripts/deploy.sh
+++ ec2/deployScripts/deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-cd /home/ec2-user
+mkdir -p /home/ec2-user/deploy
+cd /home/ec2-user/deploy
+source .env
 
-docker-compose up -d
+# Create directories needed by app
+mkdir -p ${dataPath}
+
+# Authenticate Docker with ECR
+echo "Logging in to ECR"
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+# Pull ECR images and run containers using production compose file
+echo "Pulling ECR images and deploying containers"
+docker-compose -f docker-compose-production.yml pull
+docker-compose -f docker-compose-production.yml up -d
+
+echo "Deployment complete"
--- push2ecr.sh
+++ push2ecr.sh
@@ -11,6 +11,13 @@ services=(db es rabbit redis serviceapi webapi frontend pipeline0 crawler0)
 for service in "${services[@]}"
 do
     docker-compose build $service
-    docker tag ambarec2-$service:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ambarec2-$service:latest
-    docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ambarec2-$service:latest
+    image_uri="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ambarec2-$service:latest"
+    docker tag ambarec2-$service:latest $image_uri
+    docker push $image_uri
+    echo "ambarEc2_$service=$image_uri" >> .env
 done
+
+echo "AWS_REGION=$AWS_REGION" >> .env
+echo "AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID" >> .env
+
+echo "Build complete"
