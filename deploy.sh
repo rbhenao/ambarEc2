dfbab542a7fce281919c9c570a64f6a79178751a
Ryan Henao <ryanbhenao@gmail.com>
2024-04-21 16:44:32 +0000

    moved deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Add ~/.local/bin to PATH
+mkdir -p ~/.local/bin
+export PATH=~/.local/bin:$PATH
+
+whoami
+
+# Set up deploy directory
+cd /home/ec2-user
+echo "Deploying from $(pwd)"
+
+source .env
+echo "Using ENV file:"
+cat .env
+
+# Create directories needed by app
+mkdir -p ${dataPath}
+
+# Authenticate Docker with ECR
+echo "Logging in to ECR"
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+# Pull ECR images and run containers using production compose file
+echo "Pulling ECR images and deploying containers"
+docker-compose -f docker-compose-production.yml pull
+docker-compose -f docker-compose-production.yml up -d
+
+echo "Deployment complete"
